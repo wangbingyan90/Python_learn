0497fcefce639573b24517c3667d5c7df928caee
toilet <toilet@DESKTOP-2ROBAT5>
2018-05-22 17:15:26 +0800

    完善

--- 爬虫/csdn_api.py
+++ 爬虫/csdn_api.py
@@ -22,8 +22,22 @@ def getHTMLText(url):
     获取网站源码
     url：网站地址
     '''
+    headers = {
+        'Host': 'blog.csdn.net',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0',
+        'Accept': 'application/json, text/javascript, */*; q=0.01',
+        'Accept-Language': 'zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Referer': 'https://blog.csdn.net/nav/cloud',
+        'X-Requested-With': 'XMLHttpRequest',
+        'X-Tingyun-Id': 'wl4EtIR_7Is;r=976669768',
+        'Cookie': 'uuid_tt_dd=4099473511411770564_20180522; dc_session_id=10_1526976401745.516297; TY_SESSION_ID=44a0e163-9d87-49c1-8a47-8cb70dbb5695; Hm_lvt_6bcd52f51e9b3dce32bec4a3997715ac=1526976641; Hm_lpvt_6bcd52f51e9b3dce32bec4a3997715ac=1526976651; ADHOC_MEMBERSHIP_CLIENT_ID1.0=80021b53-245d-2f0a-9e86-a5c831756bf7; dc_tos=p94eq2',
+        'Connection': 'keep-alive',
+        'Pragma': 'no-cache',
+        'Cache-Control': 'no-cache',
+    }
     try:
-        r = requests.get(url, timeout=30)
+        r = requests.get(url,headers=headers,timeout=30)
         r.raise_for_status()
         r.encoding = r.apparent_encoding
         return r.text
@@ -50,11 +64,14 @@ def parseArticle(data,r):
     '''
     for articleInfo  in data:
         url = articleInfo["url"]
+        # print(url)
         if(r.hexists("url",url)):
             continue
         r.hset("url",url,1)
         html = getHTMLText(url)
         add = re.findall(r'title-article\">(.*?)<.*?time\">(.*?)<',html,re.S)
+        if len(add)==0:
+            continue
         saveData(add[0],r)
     return 0
 
@@ -70,8 +87,8 @@ def actionOne(r):
         'Accept-Encoding': 'gzip, deflate, br',
         'Referer': 'https://blog.csdn.net/nav/cloud',
         'X-Requested-With': 'XMLHttpRequest',
-        'X-Tingyun-Id': 'wl4EtIR_7Is;r=656339184',
-        'Cookie': 'uuid_tt_dd=10_19718617050-1525588050569-254266; Hm_lvt_6bcd52f51e9b3dce32bec4a3997715ac=1525624597,1525625594,1525655453,1525656316; __yadk_uid=a4FzalfUwasDGeotovzWnloCwHwliMYe; ADHOC_MEMBERSHIP_CLIENT_ID1.0=3d3a9fe8-8ca4-c2a6-e88e-a5e9c04d7a42; dc_tos=p8c3y8; dc_session_id=10_1525656278179.889198; TY_SESSION_ID=5b696ce1-d24f-403e-9ec9-84621a795ce6; Hm_lpvt_6bcd52f51e9b3dce32bec4a3997715ac=1525656321',
+        'X-Tingyun-Id': 'wl4EtIR_7Is;r=976669768',
+        'Cookie': 'uuid_tt_dd=4099473511411770564_20180522; dc_session_id=10_1526976401745.516297; TY_SESSION_ID=44a0e163-9d87-49c1-8a47-8cb70dbb5695; Hm_lvt_6bcd52f51e9b3dce32bec4a3997715ac=1526976641; Hm_lpvt_6bcd52f51e9b3dce32bec4a3997715ac=1526976651; ADHOC_MEMBERSHIP_CLIENT_ID1.0=80021b53-245d-2f0a-9e86-a5c831756bf7; dc_tos=p94eq2',
         'Connection': 'keep-alive',
         'Pragma': 'no-cache',
         'Cache-Control': 'no-cache',
